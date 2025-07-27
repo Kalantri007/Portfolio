cec908405a3e99124a4bce13590fdc7bd68a86ab
Kalantri007 <vyankykalantri@gmail.com>
2025-07-27 15:18:13 -0700

    new temp

--- test.java
+++ test.java
@@ -2,25 +2,26 @@ public class Main {
     public static int[][] solution(int n, int m, char[] figures) {
         int[][] grid = new int[n][m];
 
-        // Define shapes for A to E
-        int[][][] shapes = {
-            {{0,0}, {1,0}},               // A: vertical 2x1
-            {{0,0}, {0,1}},               // B: horizontal 1x2
-            {{0,0}, {0,1}, {1,0}, {1,1}}, // C: 2x2 square
-            {{0,0}, {1,0}, {2,0}, {2,1}}, // D: L shape
-            {{0,1}, {1,0}, {1,1}, {1,2}}  // E: T shape
-        };
+        // Shape definitions (relative positions)
+        int[][][] shapes = new int[5][][];
+        shapes[0] = new int[][]{{0, 0}, {1, 0}};                     // A
+        shapes[1] = new int[][]{{0, 0}, {0, 1}};                     // B
+        shapes[2] = new int[][]{{0, 0}, {0, 1}, {1, 0}, {1, 1}};     // C
+        shapes[3] = new int[][]{{0, 0}, {1, 0}, {2, 0}, {2, 1}};     // D
+        shapes[4] = new int[][]{{0, 1}, {1, 0}, {1, 1}, {1, 2}};     // E
 
-        for (int idx = 0; idx < figures.length; idx++) {
-            char f = figures[idx];
-            int shapeIndex = f - 'A';
+        for (int index = 0; index < figures.length; index++) {
+            char fig = figures[index];
+            int shapeIndex = fig - 'A';
             int[][] shape = shapes[shapeIndex];
+            int value = index + 1;
+
             boolean placed = false;
 
             for (int i = 0; i < n && !placed; i++) {
                 for (int j = 0; j < m && !placed; j++) {
                     if (canPlace(grid, i, j, shape, n, m)) {
-                        placeShape(grid, i, j, shape, idx + 1);
+                        place(grid, i, j, shape, value);
                         placed = true;
                     }
                 }
@@ -30,31 +31,36 @@ public class Main {
         return grid;
     }
 
-    private static boolean canPlace(int[][] grid, int x, int y, int[][] shape, int n, int m) {
+    private static boolean canPlace(int[][] grid, int i, int j, int[][] shape, int n, int m) {
         for (int[] s : shape) {
-            int newX = x + s[0];
-            int newY = y + s[1];
-            if (newX >= n || newY >= m || grid[newX][newY] != 0) {
+            int x = i + s[0];
+            int y = j + s[1];
+            if (x < 0 || x >= n || y < 0 || y >= m || grid[x][y] != 0) {
                 return false;
             }
         }
         return true;
     }
 
-    private static void placeShape(int[][] grid, int x, int y, int[][] shape, int value) {
+    private static void place(int[][] grid, int i, int j, int[][] shape, int value) {
         for (int[] s : shape) {
-            grid[x + s[0]][y + s[1]] = value;
+            grid[i + s[0]][j + s[1]] = value;
         }
     }
 
-    // For testing
-    public static void main(String[] args) {
-        int[][] result = solution(4, 4, new char[]{'D', 'B', 'A', 'C'});
-        for (int[] row : result) {
+    // Optional: Debug printing
+    public static void print(int[][] grid) {
+        for (int[] row : grid) {
             for (int val : row) {
                 System.out.print(val + " ");
             }
             System.out.println();
         }
+    }
+
+    // Example run
+    public static void main(String[] args) {
+        int[][] result = solution(3, 3, new char[]{'D', 'A', 'A', 'A', 'A', 'A'});
+        print(result);
     }
 }
